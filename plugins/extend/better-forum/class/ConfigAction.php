e079c1a2fa493f1585007034ea17a0da802cdc5c
jDanek <jirka.danek@jdanek.eu>
2024-12-07 18:13:46 +0100

    Update version. Replace inputs, textareas and selects with system Form class methods

--- plugins/extend/better-forum/class/ConfigAction.php
+++ plugins/extend/better-forum/class/ConfigAction.php
@@ -16,27 +16,27 @@ class ConfigAction extends BaseConfigAction
         return [
             'show_icon_panel' => [
                 'label' => _lang('betterforum.config.show_icon_panel'),
-                'input' => '<input type="checkbox" name="config[show_icon_panel]" value="1"' . Form::loadCheckbox('config', $config['show_icon_panel'], 'show_icon_panel') . '>',
+                'input' => Form::input('checkbox', 'config[show_icon_panel]', '1', ['checked' => Form::loadCheckbox('config', $config['show_icon_panel'], 'show_icon_panel')]),
                 'type' => 'checkbox'
             ],
             'show_topics' => [
                 'label' => _lang('betterforum.config.show_topics'),
-                'input' => '<input type="checkbox" name="config[show_topics]" value="1"' . Form::loadCheckbox('config', $config['show_topics'], 'show_topics') . '>',
+                'input' => Form::input('checkbox', 'config[show_topics]', '1', ['checked' => Form::loadCheckbox('config', $config['show_topics'], 'show_topics')]),
                 'type' => 'checkbox'
             ],
             'show_answers' => [
                 'label' => _lang('betterforum.config.show_answers'),
-                'input' => '<input type="checkbox" name="config[show_answers]" value="1"' . Form::loadCheckbox('config', $config['show_answers'], 'show_answers') . '>',
+                'input' => Form::input('checkbox', 'config[show_answers]', '1', ['checked' => Form::loadCheckbox('config', $config['show_answers'], 'show_answers')]),
                 'type' => 'checkbox'
             ],
             'show_latest' => [
                 'label' => _lang('betterforum.config.show_latest'),
-                'input' => '<input type="checkbox" name="config[show_latest]" value="1"' . Form::loadCheckbox('config', $config['show_latest'], 'show_latest') . '>',
+                'input' => Form::input('checkbox', 'config[show_latest]', '1', ['checked' => Form::loadCheckbox('config', $config['show_latest'], 'show_latest')]),
                 'type' => 'checkbox'
             ],
             'show_latest_answers' => [
                 'label' => _lang('betterforum.config.show_latest_answers'),
-                'input' => '<input type="checkbox" name="config[show_latest_answers]" value="1"' . Form::loadCheckbox('config', $config['show_latest_answers'], 'show_latest_answers') . '>',
+                'input' => Form::input('checkbox', 'config[show_latest_answers]', '1', ['checked' => Form::loadCheckbox('config', $config['show_latest_answers'], 'show_latest_answers')]),
                 'type' => 'checkbox'
             ],
             'pos_latest_answers' => [
